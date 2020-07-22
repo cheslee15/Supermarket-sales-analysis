f0d4b3eab9c22d4406a8c63d4265b0f619fb3084
mpzheng <52969917+mpzheng@users.noreply.github.com>
2020-07-22 11:04:06 +0800

    update dataset

--- dataset/read_dataset.py
+++ dataset/read_dataset.py
@@ -5,10 +5,10 @@ data = pd.read_csv('2013_small.csv', usecols=[1, 2, 3, 4, 5, 7, 8, 9, 12])
 party=[]
 #读入10个对应商店的数据为dataframe格式
 for i in range(10):
-    party.append(data.loc[data.store_nbr==i+11])
+    party.append(data.loc[data.store_nbr==i+10])
     #下面一行代码可生成10个文件，对应相应的party
     #party[i].to_csv('party%s.csv'%(str(i)))
 
-print(party[0])
+print(party[0]['date'])
 
 
