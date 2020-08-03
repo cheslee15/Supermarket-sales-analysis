b7aa446d86aa91d655bf05364b6783fe3c84dccd
mpzheng <52969917+mpzheng@users.noreply.github.com>
2020-08-03 18:38:18 +0800

    update dataset

--- dataset/read_dataset.py
+++ dataset/read_dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 #usecols选取相应的列
 data = pd.read_csv('2013_small.csv', usecols=[1, 2, 3, 4, 5, 7, 8, 9, 12])
-states=['Pichincha','Cotopaxi','Chimborazo','Imbabura','Santo' 'Domingo de los Tsachilas','Bolivar']
+states=['Pichincha','Cotopaxi','Chimborazo','Imbabura','Santo Domingo de los Tsachilas','Bolivar']
 party=[]
 #读入10个对应商店的数据为dataframe格式
 for i in range(6):
